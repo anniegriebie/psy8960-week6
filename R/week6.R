c3b232ee15b0ed2a6a3b8738752b87b181b700a6
anniegriebie <GRIEB046@umn.edu>
2023-02-25 13:12:14 -0600

    Data Import

--- R/week6.R
+++ R/week6.R
@@ -2,3 +2,9 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(stringi)
 library(tidyverse)
+
+# Data Import
+citations <- stri_read_lines("../data/citations.txt")
+citations_txt <- citations[!stri_isempty(citations)]
+length(citations) - length(citations_txt)
+mean(str_length(citations_txt))
