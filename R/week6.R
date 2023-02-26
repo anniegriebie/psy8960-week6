5d09d27aa1b88cc1b37ae76ced5b64228f2efba8
anniegriebie <GRIEB046@umn.edu>
2023-02-26 16:28:01 -0600

    Final Commit

--- R/week6.R
+++ R/week6.R
@@ -15,10 +15,11 @@ mean(str_length(citations_txt))
 sample(citations_txt, size = 10)
 
 citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
-  mutate(cite = str_replace_all(string = cite, pattern = "\"|\'", replacement = "")) %>%
-  mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2])
+  mutate(cite = str_replace_all(cite, pattern = "\"|\'", replacement = "")) %>%
+  mutate(year = str_match(cite, pattern = "\\((\\d{4})\\)\\.")[,2]) %>%
+  mutate(page_start = str_match(string = cite, pattern = "([\\d]+)-")[,2]) %>%
+  mutate(perf_ref = str_detect(cite, pattern = fixed("performance", ignore_case = TRUE))) %>%
+  mutate(title = str_match(cite, pattern = "\\d{4}\\)\\.\\s*([A-Z][^.]+\\.)\\s")[,2]) %>%
+  mutate(first_author = str_match(cite, pattern = "^\\*?([A-Z][a-zA-Z-?\\s*]+\\,?\\s*[A-Z]\\.?\\s*[A-Z]?\\.?\\s*?[A-Z]?\\.?)")[,2])
   
-
-
-
 sum(!is.na(citations_tbl$first_author))
