4e6c4eb7c4d116dd21cec5edf13524670bd577c5
anniegriebie <GRIEB046@umn.edu>
2023-02-25 20:29:50 -0600

    Added windows encoding into code rather than using drop down and started data cleaning

--- R/week6.R
+++ R/week6.R
@@ -6,10 +6,13 @@ library(tidyverse)
 
 
 # Data Import
-citations <- stri_read_lines("../data/citations.txt")
+citations <- stri_read_lines("../data/citations.txt", encoding = "WINDOWS-1252")
 citations_txt <- citations[!stri_isempty(citations)]
 length(citations) - length(citations_txt)
 mean(str_length(citations_txt))
 
 # Data Cleaning
 sample(citations_txt, size = 10)
+
+citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>%
+  mutate(cite = str_replace_all(string = cite, pattern = "\"|\'", replacement = ""))
